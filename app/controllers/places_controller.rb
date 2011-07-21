8b42f39c256a521000b6824af419d14d7f933407
Matt Moriarity <matt@mattmoriarity.com>
2011-07-21 16:05:08 -0400

    Only places for the current user's group are now loaded. #30

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,19 +3,19 @@ class PlacesController < ApplicationController
   before_filter :login_required
   
   def index
-    @places = Place.all
+    @places = @current_user.group.places
     @place = Place.new
     respond_with(@places)
   end
   
   def periodic
-    @places = Place.all
+    @places = @current_user.group.places
     render :partial => 'places'
   end
   
   def create
-    @place = Place.create(params[:place].merge(:person => session[:user]))
-    @places = Place.all
+    @place = Place.create(params[:place].merge(:person => @current_user))
+    @places = @current_user.group.places
   end
   
   def edit
@@ -27,11 +27,11 @@ class PlacesController < ApplicationController
     @place.attributes = params[:place]
     @place.save
     
-    @places = Place.all
+    @places = @current_user.group.places
   end
   
   def destroy
-    @place = Place.first(:conditions => {:id => params[:id], :person_id => session[:user]})
+    @place = Place.first(:conditions => {:id => params[:id], :person_id => @current_user})
     @place.destroy
     respond_with(@place)
   end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ActiveRecord::Base
   belongs_to :created_by, class_name: "Person"
   has_many :memberships
   has_many :people, through: :memberships
+  
+  # Group places are all the places created by people in the group
+  has_many :places, through: :people, source: :places 
 end
