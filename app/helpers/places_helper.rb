d2b1fdf64290b90ba8dba43a8a64246f68a99ff2
Matt Moriarity <matt@mattmoriarity.com>
2012-01-28 20:13:07 -0500

    Forgot to show vote comments on walking places as well
    Also cleaned up some formatting. #37

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,9 @@
 module PlacesHelper
+  def vote_comment(vote)
+    if vote.person == @current_user
+      link_to t('.comment'), edit_vote_path(vote), remote: true, class: 'comment-button'
+    elsif !vote.comment.blank?
+      tag :span, {class: 'vote-comment ui-icon-comment ui-icon', title: vote.comment}, true
+    end
+  end
 end
