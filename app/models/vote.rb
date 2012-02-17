68edc2afed5331acbbf23d11d2424ad2ada863b8
Matt Moriarity <matt@mattmoriarity.com>
2012-02-17 10:21:14 -0500

    Fixed a problem with voting caused by previous comment fix.
    #40

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class SeatCountValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     # don't care if we were already in the car
-    return if value.votes.include?(record)
+    return if value and value.votes.include?(record)
     
     if value and value.seats_left(record.place) < 1
       record.errors.add attribute, "has no more seats available." #TODO i18n
