280d2bd88422a888190d225f7139adbd2d1df0a9
mmoriarity3 <matthew.moriarity@gtri.gatech.edu>
2012-04-20 15:58:55 -0400

    Added a counter cache to votes.
    This makes it possible to more easily sort places by vote count.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class SeatCountValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     # don't care if we were already in the car
     return if value and value.votes.include?(record)
-    
+
     if value and value.seats_left(record.place) < 1
       record.errors.add attribute, "has no more seats available." #TODO i18n
     end
@@ -10,15 +10,15 @@ class SeatCountValidator < ActiveModel::EachValidator
 end
 
 class Vote < ActiveRecord::Base
-  belongs_to :place
+  belongs_to :place, counter_cache: true
   belongs_to :person
   belongs_to :car
-  
+
   validates :person_id, :uniqueness => true # TODO be able to store history
   validates :car, :seat_count => true
-  
+
   before_create :assign_car_owner
-  
+
   private
     def assign_car_owner
       self.car = person.car if person.has_car?
---
+++ db/migrate/20120420191724_add_vote_count.rb
@@ -0,0 +1,10 @@
+class AddVoteCount < ActiveRecord::Migration
+  def change
+    add_column :places, :votes_count, :integer
+
+    Place.reset_column_information
+    Place.all.each do |p|
+      Place.update_counters p.id, votes_count: p.votes.count
+    end
+  end
+end
